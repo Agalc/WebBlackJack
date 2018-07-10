df8827f921e62b8f6a85237e0ef2fd8ef4855944
Alcest <alcestoneone@gmail.com>
2018-07-10 16:47:47 +0300

    UnitOfWork pattern added

---
+++ BlackJack/BlackJack.DAL/Enteties/EntityUnitOfWork.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using BlackJack.DAL.Interfaces;
+using BlackJack.DAL.Repositories;
+
+namespace BlackJack.DAL.Enteties
+{
+  class EntityUnitOfWork : IUnitOfWork
+  {
+
+    private readonly BjContext _context;
+    private UserRepository _userRepository;
+    private GameRepository _gameRepository;
+    private RoundRepository _roundRepository;
+    private CardRepository _cardRepository;
+
+    public EntityUnitOfWork(string connectionString)
+    {
+      _context = new BjContext(connectionString);
+    }
+
+    public IRepository<User> Users
+    {
+      get
+      {
+        if (_userRepository == null)
+          _userRepository = new UserRepository(_context);
+        return _userRepository;
+      }
+    }
+
+    public IRepository<Game> Games
+    {
+      get
+      {
+        if (_gameRepository == null)
+          _gameRepository = new GameRepository(_context);
+        return _gameRepository;
+      }
+    }
+
+    public IRepository<Round> Rounds
+    {
+      get
+      {
+        if (_roundRepository == null)
+          _roundRepository = new RoundRepository(_context);
+        return _roundRepository;
+      }
+    }
+
+    public IRepository<Card> Cards
+    {
+      get
+      {
+        if (_cardRepository == null)
+          _cardRepository = new CardRepository(_context);
+        return _cardRepository;
+      }
+    }
+
+    public void Save()
+    {
+      _context.SaveChanges();
+    }
+
+    private bool _disposed;
+
+    public virtual void Dispose(bool disposing)
+    {
+      if (!_disposed)
+      {
+        if (disposing)
+        {
+          _context.Dispose();
+        }
+        _disposed = true;
+      }
+    }
+
+    public void Dispose()
+    {
+      Dispose(true);
+      GC.SuppressFinalize(this);
+    }
+  }
+}
---
+++ BlackJack/BlackJack.DAL/Interfaces/IUnitOfWork.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using BlackJack.DAL.Enteties;
+
+namespace BlackJack.DAL.Interfaces
+{
+  public interface IUnitOfWork : IDisposable
+  {
+    IRepository<User> Users { get; }
+    IRepository<Card> Cards { get; }
+    IRepository<Game> Games { get; }
+    IRepository<Round> Rounds { get; }
+
+    void Save();
+  }
+}
