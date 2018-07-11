d6e80acf8604c112421a9966a34fb82da1e6781e
Alcest <alcestoneone@gmail.com>
2018-07-11 14:06:52 +0300

    HomeController cleanup

--- BlackJack/BlackJack/Controllers/HomeController.cs
+++ BlackJack/BlackJack/Controllers/HomeController.cs
@@ -2,49 +2,30 @@
 using System.Linq;
 using System.Web.Mvc;
 using BlackJack.DAL.Enteties;
+using BlackJack.DAL.Interfaces;
 
 namespace BlackJack.Controllers
 {
   public class HomeController : Controller
   {
-    private static readonly string _connectionString = "Default";
-    private readonly UnitOfWork _unitOfWork;
+    private const string ConnectionString = "Default";
+    private readonly IUnitOfWork _unitOfWork;
 
     public HomeController()
     {
-      _unitOfWork = new UnitOfWork(_connectionString);
+      _unitOfWork = new UnitOfWork(ConnectionString);
     }
 
     public ActionResult Index()
     {
-      ViewBag.User = _unitOfWork.Users.GetAll().ToList();
-
+      ViewBag.User = _unitOfWork.Users.GetAll();
       return View();
     }
 
     [HttpGet]
     public ActionResult Hand(int id)
     {
-      //var commentsOfMember = _unitOfWork.Users
-      //  .Where(us => us.Id == id)
-      //  .Select(us => us.Cards)
-      //  .ToList();
-      //ViewBag.UserId = id;
       ViewBag.Cards = _unitOfWork.Cards.GetAll();
-      return View();
-    }
-
-    public ActionResult About()
-    {
-      ViewBag.Message = "Your application description page.";
-
-      return View();
-    }
-
-    public ActionResult Contact()
-    {
-      ViewBag.Message = "Your contact page.";
-
       return View();
     }
   }
